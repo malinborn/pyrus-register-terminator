2ce347e0f31ceab7c886d0f190c8a1c6c87bac1c
malinborn <rikkeisy@gmail.com>
2022-11-05 15:53:53 +0300

    added: terminate tickets function

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from pyrus_client import PyrusClient
 from equalizer import Equalizer
 
 
+HOUR_PAIRS_OF_DAY = (("00", "03"), ("04", "07"), ("08", "11"), ("12", "15"), ("16", "19"), ("20", "23"))
+FORM_IDS_TO_CLEAN = [522023]  # TODO: make config and read it from it
+
+
 def get_dates_to_terminate() -> list[date]:
     with DatesLogger() as log_manager:
         start_date: date = __calculate_start_date(log_manager.read_logs())
@@ -34,19 +38,20 @@ def inspect(dates: list[date]) -> None:
     print("#" * 30)
     pprint(dates[len(dates) - 100::])
     print("#" * 30)
-    input("IF DATES ARE CORRECT - PRESS ENTER     \n"
-          "                                       \n"
-          "       !!!!!!CAUTION!!!!!!             \n"
-          "       !!!!!!CAUTION!!!!!!             \n"
-          "       !!!!!!CAUTION!!!!!!             \n"
-          "                                       \n"
-          "IT WILL LAUNCH TICKET TERMINATION PROCESS")
+    input("IF DATES ARE CORRECT - PRESS ENTER       \n"
+          "                                         \n"
+          "       !!!!!!CAUTION!!!!!!               \n"
+          "       !!!!!!CAUTION!!!!!!               \n"
+          "       !!!!!!CAUTION!!!!!!               \n"
+          "                                         \n"
+          "IT WILL LAUNCH TICKET TERMINATION PROCESS\n")
 
 
 def terminate_tickets(days: list[date]):
     pyrus_client = __configure_pyrus_client("creds.txt")
     with DatesLogger() as log_manager:
         for day in days:
+            logger.info(f"terminating tickets of {day.isoformat()}...")
             __terminate_tickets_by(day, pyrus_client, log_manager)
             logger.info(f"ticket of {day.isoformat()} are terminated")
             log_manager.write(day)
@@ -65,20 +70,46 @@ def __configure_pyrus_client(path: str) -> PyrusClient:
 
 
 def __terminate_tickets_by(day: date, pyrus_client: PyrusClient, log_manager: DatesLogger) -> None:
+    logger.info(f"collecting tickets for {day.isoformat()}")
     ticket_ids = __get_ticket_ids(day, pyrus_client)
     for ticket_id in ticket_ids:
         with Equalizer(seconds=1, log_manager=log_manager):
             pyrus_client.delete_ticket(ticket_id)
+            logger.debug(f"ticket with id {ticket_id} deleted")
 
 
-def __get_ticket_ids(day: date, pyrus_client: PyrusClient) -> Generator[int]:
-    ...
+def __get_ticket_ids(day: date, pyrus_client: PyrusClient) -> Generator:
+    tickets_json = list()
+    for form_id in FORM_IDS_TO_CLEAN:
+        logger.info(f"collecting tickets of {form_id}")
+        tickets_json.extend(__collect_tickets(day, form_id, pyrus_client))
+        logger.info(f"tickets of {form_id} are collected")
+    return (x["id"] for x in tickets_json)
+
+
+def __collect_tickets(day, form_id, pyrus_client):
+    collected_tickets = list()
+    for hour_pair in HOUR_PAIRS_OF_DAY:
+        logger.info(f"collecting tickets from {hour_pair[0]} to {hour_pair[1]}...")
+        try:
+            collected_tickets.extend(pyrus_client.get_register(form_id, day, hour_pair)["tasks"])
+            logger.info("tickets collected")
+        except Exception as ex:
+            logger.error(ex)
+    return collected_tickets
 
 
 def main():
     dates_to_delete = get_dates_to_terminate()
     inspect(dates_to_delete)
     terminate_tickets(dates_to_delete)
+
+
+def test_main():
+    pyrus_client = __configure_pyrus_client("creds.txt")
+    ids = __get_ticket_ids(date.today() - timedelta(days=1), pyrus_client)
+    for i in range(100):
+        print(ids.__next__())
 
 
 if __name__ == "__main__":
